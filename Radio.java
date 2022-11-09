44d2eccdad63bc78f513380b60b6a5b21aa08583
Rodrimansidub14 <98109931+Rodrimansidub14@users.noreply.github.com>
2022-11-08 22:11:29 -0600

    Clases Radio y Reproducci[on

---
+++ Radio.java
@@ -0,0 +1,9 @@
+public class Radio {
+    private String Banda;
+    private float Frecuencia;
+    private float Emisoras[];
+
+
+
+    
+}
--- Reproduccion.java
+++ Reproduccion.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+
 public class Reproduccion {
 public ArrayList<Cancion> Playlist;
+
+
+    public Reproduccion() {
+    }
+
+
+    public Reproduccion(ArrayList<Cancion> Playlist) {
+        this.Playlist = Playlist;
+    }
+
+
+
+    public ArrayList<Cancion> getPlaylist() {
+        return this.Playlist;
+    }
+
+    public void setPlaylist(ArrayList<Cancion> Playlist) {
+        this.Playlist = Playlist;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " Playlist='" + getPlaylist() + "'" +
+            "}";
+    }
 
 }
