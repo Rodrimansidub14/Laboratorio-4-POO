4c4bc9bd622893e5e4a471360e4eb3679fcbc222
Rodrimansidub14 <98109931+Rodrimansidub14@users.noreply.github.com>
2022-11-08 22:12:28 -0600

    Update Radio.java

--- Radio.java
+++ Radio.java
@@ -1,9 +1,50 @@
 public class Radio {
     private String Banda;
     private float Frecuencia;
-    private float Emisoras[];
+    double [] emisoras = new double[] {};
 
 
+    public Radio() {
+    }
 
-    
+
+    public Radio(String Banda, float Frecuencia, double[] emisoras) {
+        this.Banda = Banda;
+        this.Frecuencia = Frecuencia;
+        this.emisoras = emisoras;
+    }
+
+    public String getBanda() {
+        return this.Banda;
+    }
+
+    public void setBanda(String Banda) {
+        this.Banda = Banda;
+    }
+
+    public float getFrecuencia() {
+        return this.Frecuencia;
+    }
+
+    public void setFrecuencia(float Frecuencia) {
+        this.Frecuencia = Frecuencia;
+    }
+
+    public double[] getEmisoras() {
+        return this.emisoras;
+    }
+
+    public void setEmisoras(double[] emisoras) {
+        this.emisoras = emisoras;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " Banda='" + getBanda() + "'" +
+            ", Frecuencia='" + getFrecuencia() + "'" +
+            ", emisoras='" + getEmisoras() + "'" +
+            "}";
+    }
+  
 }
